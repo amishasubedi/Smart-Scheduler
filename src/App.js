4f8b8b1914c4d46895c63479c9f82e80ac234ba2
asubedi <asubedi@go.olemiss.edu>
2022-10-15 01:34:53 -0500

    main header with calendar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import { Calendar } from "react-calendar";
 import "./App.css";
 import Login from "./components/Login/Login";
 import MainNavigation from "./components/Navigation/MainNavigation";
 import Navlinks from "./components/Navigation/Navlinks";
+import CalendarInfo from "./components/Calendar/CalendarInfo";
 
 function App() {
   return (
     <div>
-      {/* <MainNavigation />
-      <Login /> */}
-      <Calendar />
+      <MainNavigation />
+      <CalendarInfo />
     </div>
   );
 }
