23e9d5b95fa5f0210e8a70135f8d9ccab657a549
asubedi <asubedi@go.olemiss.edu>
2022-10-14 19:47:37 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div>
+      {/* <h2>First App</h2> */}
+      <Login />
+    </div>
+  );
+}
+
+export default App;
